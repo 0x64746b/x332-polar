4942f43d2311a6f22e4ca69faccd4af282377e03
dtk <dtk@gmx.de>
2025-01-28 18:54:09 +0100

    feat: calculate average run angles

--- aggregate_speeds.py
+++ aggregate_speeds.py
@@ -15,11 +15,14 @@ for entry in x332s:
     boat_speeds['beat_angle'].append(entry['vpp']['beat_angle'])
     boat_speeds['beating'].append((entry['vpp']['beat_vmg'] / np.cos(np.deg2rad(entry['vpp']['beat_angle']))).tolist())
 
+    boat_speeds['run_angle'].append(entry['vpp']['run_angle'])
+
 avg_boat_speeds = { angle: np.mean(boat_speeds[angle], axis=0).tolist() for angle in boat_speeds}
-avg_boat_speeds['beat_angle'] = np.mean(boat_speeds['beat_angle'], axis=0).tolist()
-avg_boat_speeds['beating'] = np.mean(boat_speeds['beating'], axis=0).tolist()
 avg_boat_speeds['angles'] = entry['vpp']['angles']
 avg_boat_speeds['speeds'] = entry['vpp']['speeds']
+avg_boat_speeds['beat_angle'] = np.mean(boat_speeds['beat_angle'], axis=0).tolist()
+avg_boat_speeds['beating'] = np.mean(boat_speeds['beating'], axis=0).tolist()
+avg_boat_speeds['run_angle'] = np.mean(boat_speeds['run_angle'], axis=0).tolist()
 
 with open('output/avg_x332.json', 'w') as avg_x332:
     json.dump(avg_boat_speeds, avg_x332, indent=4)

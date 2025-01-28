6ad228035640b4d9be919f45058b14977636ba83
dtk <dtk@gmx.de>
2025-01-28 14:01:00 +0100

    refactor: remove unused code

--- aggregate_speeds.py
+++ aggregate_speeds.py
@@ -1,24 +1,7 @@
 from collections import defaultdict
-from glob import glob
-from csv import reader, DictReader
-from importlib.metadata import entry_points
-
-import numpy as np
-import pandas as pd
 import json
 
-from numpy.f2py.crackfortran import entrypattern
-
-#for csv in glob('data/*.csv'):
-#    print(csv)
-#    with open(csv) as csv_file:
-#        x332s = pd.read_csv(csv, delimiter=';').drop(0)
-#        if x332s.shape != (24, 9):
-#            print(f'{csv} has an unexpected structure: {x332s.shape}')
-#        #csv_reader = reader(csv_file, delimiter=';')
-#        #csv_reader = DictReader(csv_file, delimiter=';')
-#        #for row in csv_reader:
-#        #    print(row)
+import numpy as np
 
 with open('data/orc_2024.valid.json') as all_boats:
     x332s = [entry for entry in json.load(all_boats) if entry['boat']['type'] == 'X-332']

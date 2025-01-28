aae14ab0d67236350060fb884fef270dffafa8fd
dtk <dtk@gmx.de>
2025-01-28 14:16:12 +0100

    feat: average the beat angles

--- aggregate_speeds.py
+++ aggregate_speeds.py
@@ -12,7 +12,11 @@ for entry in x332s:
     for angle in entry['vpp']['angles']:
         boat_speeds[angle].append(entry['vpp'][str(angle)])
 
+    boat_speeds['beat_angle'].append(entry['vpp']['beat_angle'])
+
+
 avg_boat_speeds = { angle: np.mean(boat_speeds[angle], axis=0).tolist() for angle in boat_speeds}
+avg_boat_speeds['beat_angle'] = np.mean(boat_speeds['beat_angle'], axis=0).tolist()
 avg_boat_speeds['angles'] = entry['vpp']['angles']
 avg_boat_speeds['speeds'] = entry['vpp']['speeds']
 
